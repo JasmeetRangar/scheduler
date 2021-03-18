d40c35d42ea65abbbd736149d1c2fe7659baecc3
Jasmeet Rangar <jasmeetsinghr@gmail.com>
2021-03-18 19:23:50 -0400

    Add useVisualMode to Empty and Show in Appointment'

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,34 @@
 import React from "react";
+import useVisualMode from "hooks/useVisualMode";
 import "./styles.scss";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
-import Header from "components/Appointment/Header"
+import Header from "components/Appointment/Header";
+
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
 export default function Appointment(props) {
-  const interview = props.interview;
+   const { mode, transition, back } = useVisualMode(
+      props.interview ? SHOW : EMPTY
+    );
+  
   return (
     <article className="appointment">
         {props.time}
          <Header
             time={props.time}
          />
-         {props.interview ?
+                  {mode === EMPTY && (
+            <Empty
+               onAdd={() => console.log("Clicked onAdd")} 
+            />
+         )}
+         {mode === SHOW && (
             <Show
                student={props.interview.student}
                interviewer={props.interview.interviewer} 
-            /> : <Empty />}
+            />
+         )}
       </article>
   )
 }
